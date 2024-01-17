5a1e04c0943c4c97482cee76809efcf36a1274d5
12paul123 <49698178+12paul123@users.noreply.github.com>
2024-01-17 17:19:31 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Paul Schmidt on 2024-01-17.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
