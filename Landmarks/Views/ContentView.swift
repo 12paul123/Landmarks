cef2758e11c93f00ecd0333e43ece930700c5a8c
12paul123 <49698178+12paul123@users.noreply.github.com>
2024-01-18 20:33:31 +0100

    s

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Paul Schmidt on 2024-01-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
