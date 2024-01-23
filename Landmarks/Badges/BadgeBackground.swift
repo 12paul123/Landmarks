1ccdc753f2f953ca496616cbd1843d3df804a5ea
12paul123 <49698178+12paul123@users.noreply.github.com>
2024-01-23 12:58:07 +0100

    s

--- Landmarks/Badges/BadgeBackground.swift
+++ Landmarks/Badges/BadgeBackground.swift
@@ -22,7 +22,8 @@ struct BadgeBackground: View {
                         y: height * (0.20 + HexagonParameters.adjustment)
                     )
                 )
-                
+
+
                 HexagonParameters.segments.forEach { segment in
                     path.addLine(
                         to: CGPoint(
@@ -30,7 +31,8 @@ struct BadgeBackground: View {
                             y: height * segment.line.y
                         )
                     )
-                    
+
+
                     path.addQuadCurve(
                         to: CGPoint(
                             x: width * segment.curve.x + xOffset,
@@ -47,14 +49,13 @@ struct BadgeBackground: View {
                 Gradient(colors: [Self.gradientStart, Self.gradientEnd]),
                 startPoint: UnitPoint(x: 0.5, y: 0),
                 endPoint: UnitPoint(x: 0.5, y: 0.6)
-             ))
+            ))
         }
-        .aspectRatio(1, contentMode: .fit)
     }
-    
     static let gradientStart = Color(red: 239.0 / 255, green: 120.0 / 255, blue: 221.0 / 255)
     static let gradientEnd = Color(red: 239.0 / 255, green: 172.0 / 255, blue: 120.0 / 255)
 }
+
 
 #Preview {
     BadgeBackground()
